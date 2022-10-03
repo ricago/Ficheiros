13c739102c8f84e4f1c3fa0deb9e887668ca95be
Ricardo Henriques <ricardohenriques7410@gmail.com>
2022-10-03 15:23:09 +0100

    Cria Ficheiros

--- Manipula_Ficheiros/src/manipula_ficheiros/Ficheiro.java
+++ Manipula_Ficheiros/src/manipula_ficheiros/Ficheiro.java
@@ -1,10 +1,25 @@
 package manipula_ficheiros;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class Ficheiro {
 
     static void criaFicheiro() {
+        String nome;
+        System.out.println("Insira o nome do ficheiro a criar");
+        nome = Manipula_Ficheiros.ler.nextLine();
+        nome = Manipula_Ficheiros.ler.next();
+        File ficheiro = new File(nome);
+        if(!ficheiro.exists()){
+            try {
+                ficheiro.createNewFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     static void verificaSeExiste() {
