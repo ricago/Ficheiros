c75ba212bd1db8fca14462cd4dbd4ce87c6de23c
Ricardo Henriques <ricardohenriques7410@gmail.com>
2022-10-03 16:02:07 +0100

    Criar ou adicionar texto a ficheiros

--- Manipula_Ficheiros/src/manipula_ficheiros/Ficheiro.java
+++ Manipula_Ficheiros/src/manipula_ficheiros/Ficheiro.java
@@ -1,6 +1,8 @@
 package manipula_ficheiros;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,11 +25,46 @@ class Ficheiro {
     }
 
     static void verificaSeExiste() {
+         String nome;
+        System.out.println("Insira o nome do ficheiro a criar");
+        nome = Manipula_Ficheiros.ler.nextLine();
+        nome = Manipula_Ficheiros.ler.next();
+        File ficheiro = new File(nome);
+        if(!ficheiro.exists()){
+            System.out.println("O ficheiro não existe");
+         }else{
+             System.out.println("O ficheiro existe");
+        }
     }
 
     static void escreveNoFinalFicheiro() {
-    }
-
+        String nome;
+        System.out.println("Insira o nome do ficheiro a criar");
+        nome = Manipula_Ficheiros.ler.nextLine();
+        nome = Manipula_Ficheiros.ler.next();
+        File ficheiro = new File(nome);
+       
+            try {
+                if(!ficheiro.exists()){
+                    ficheiro.createNewFile();
+         
+                } 
+                
+                FileWriter fw = new FileWriter(ficheiro,true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                String texto;
+                System.out.println("Insira o conteudo");
+                texto = Manipula_Ficheiros.ler.nextLine();
+                texto = Manipula_Ficheiros.ler.nextLine();
+                bw.write(texto);
+                bw.newLine();
+                bw.close();
+                fw.close();
+            }catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            }
+    
     static void escreveFicheiroNovo() {
     }
 
