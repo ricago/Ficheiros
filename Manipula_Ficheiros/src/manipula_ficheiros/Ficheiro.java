7ec4b99fca2a76c5b8017c9abcc9c6ddf04edbbe
Ricardo Henriques <ricardohenriques7410@gmail.com>
2022-10-03 16:55:30 +0100

    Mostra conteudo do ficheiro e adiciona

--- Manipula_Ficheiros/src/manipula_ficheiros/Ficheiro.java
+++ Manipula_Ficheiros/src/manipula_ficheiros/Ficheiro.java
@@ -1,7 +1,10 @@
 package manipula_ficheiros;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -50,7 +53,7 @@ class Ficheiro {
          
                 } 
                 
-                FileWriter fw = new FileWriter(ficheiro,true);
+                FileWriter fw = new FileWriter(ficheiro);
                 BufferedWriter bw = new BufferedWriter(fw);
                 String texto;
                 System.out.println("Insira o conteudo");
@@ -66,9 +69,55 @@ class Ficheiro {
             }
     
     static void escreveFicheiroNovo() {
+        
+        String nome;
+        System.out.println("Insira o nome do ficheiro a criar :");
+        nome = Manipula_Ficheiros.ler.nextLine();
+        nome = Manipula_Ficheiros.ler.next();
+        File ficheiro = new File (nome);
+        try { 
+            if(!ficheiro.exists()){         
+                ficheiro.createNewFile();  
+                }  
+                FileWriter fw = new FileWriter(ficheiro, true); 
+                BufferedWriter bw = new BufferedWriter (fw); 
+                String texto; 
+                System.out.println("Insira o conteudo"); 
+                texto = Manipula_Ficheiros.ler.nextLine(); 
+                texto = Manipula_Ficheiros.ler.nextLine(); 
+                bw.write(texto); 
+                bw.newLine(); 
+                bw.close(); 
+                fw.close(); 
+            } catch (IOException ex){ 
+                ex.printStackTrace(); } 
     }
 
     static void mostraConteudoFicheiro() {
+         String nome;
+        System.out.println("Insira o nome do ficheiro a verificar :");
+        nome = Manipula_Ficheiros.ler.nextLine();
+        nome = Manipula_Ficheiros.ler.next();
+        File ficheiro = new File (nome);
+        if(!ficheiro.exists()){
+            System.out.println("Oficheiro não existe");
+        }else{
+            
+            try{
+                FileReader fr = new FileReader (ficheiro);
+                BufferedReader br = new BufferedReader (fr);
+                while (br.ready()){
+                    String linha = br.readLine ();
+                    System.out.println (linha);
+                }
+                br.close();
+                br.close();
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            } catch (IOException ioe) {
+                
+            }
+        }
     }
 
     static void mostraListaFicheiros() {
